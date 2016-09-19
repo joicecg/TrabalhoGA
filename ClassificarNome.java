fdd11dddf89fed94ad6ea630c4afeb16482cec03
Anderson Cossul <anderson_cossul@hotmail.com>
2016-09-19 15:37:08 -0300

    Add files via upload

---
+++ ClassificarNome.java
@@ -0,0 +1,9 @@
+package AndersonDaSilvaCossulJoyceCazanoskiGomesTrabGA;
+
+public ClassificarNome implements Comparator {
+	
+	@Override
+	public int compare(Object piloto1, Ojbect piloto2) {
+		return piloto1.getNome().compareTo(piloto2.getNome());
+	}
+}
