ec4cd3bcb5b0cebbe6dace940c80813835fc403b
Anderson Cossul <anderson_cossul@hotmail.com>
2016-09-19 15:47:04 -0300

    Update ClassificarPontos.java

--- ClassificarPontos.java
+++ ClassificarPontos.java
@@ -1,6 +1,6 @@
 package AndersonDaSilvaCossulJoyceCazanoskiGomesTrabGA;
 
-public class ClassificarPontos implements Comparator{
+public class ClassificarPontos implements Comparator { 
 	@Override
 	public int compare(Object piloto1, Object piloto2) {
 		return ((Piloto)piloto1).getPontos() - ((Piloto)piloto2).getPontos(); //retorna valor negativo caso p1 tiver menos pontos
