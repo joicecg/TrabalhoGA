4a828006b3ce31cda3f4ccb88f88edeb2c3507d0
Anderson Cossul <anderson_cossul@hotmail.com>
2016-09-21 11:17:12 -0300

    Update ClassificarNome.java

--- ClassificarNome.java
+++ ClassificarNome.java
@@ -2,7 +2,7 @@ package AndersonDaSilvaCossulJoyceCazanoskiGomesTrabGA;
 
 public class ClassificarNome implements Comparator {
 	@Override
-	public int compare(Object piloto1, Ojbect piloto2) {
+	public int compare(Object piloto1, Object piloto2) {
 		return ((Piloto)piloto1).getNome().compareTo(((Piloto)piloto2).getNome());
 	}
 }
