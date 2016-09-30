4c77ee02524a088c4ebd430ed7bf8d859c208a6c
Anderson Cossul <anderson_cossul@hotmail.com>
2016-09-30 15:54:09 -0300

    Delete ClassificarNome.java

--- ClassificarNome.java
+++
@@ -1,8 +0,0 @@
-package AndersonDaSilvaCossulJoyceCazanoskiGomesTrabGA;
-
-public class ClassificarNome implements Comparator<Piloto> {
-	@Override
-	public int compare(Piloto piloto1, Piloto piloto2) {
-		return piloto1.getDriver().compareTo(piloto2.getDriver());
-	}
-}
