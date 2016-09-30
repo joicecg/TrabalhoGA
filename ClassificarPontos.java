1c6e59b58ed02c7c5a2d7de9454060ea72a80c25
Anderson Cossul <anderson_cossul@hotmail.com>
2016-09-30 15:56:23 -0300

    Delete ClassificarPontos.java

--- ClassificarPontos.java
+++
@@ -1,8 +0,0 @@
-package AndersonDaSilvaCossulJoyceCazanoskiGomesTrabGA;
-
-public class ClassificarPontos implements Comparator<Piloto> { 
-	@Override
-	public int compare(Piloto piloto1, Piloto piloto2) {
-		return piloto1.getPts() - piloto2.getPts(); //retorna valor negativo caso p1 tiver menos pontos
-	}
-}
