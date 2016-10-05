f42bc7f42915bbc8c239c30b9df7ac45b89f0c86
Joice Cazanoski Gomes <cgjoice@gmail.com>
2016-10-05 16:50:56 -0300

    Update porNome.java

--- porNome.java
+++ porNome.java
@@ -6,6 +6,6 @@ public class porNome implements Comparator<Piloto> {
 	@Override
 	public int compare(Piloto piloto1, Piloto piloto2) {
 		if(piloto1 == null) return 0;
-			return piloto1.getDriver().compareTo(piloto2.getDriver());
+		return piloto1.getDriver().compareTo(piloto2.getDriver());
 	}
 }
